e6150449ae63a691aaea0030196a416d2dcb0814
Princepiax <156066814+Princepiax@users.noreply.github.com>
2024-01-09 17:38:39 +0530

    Update lo.py

--- lo.py
+++ lo.py
@@ -70,9 +70,9 @@ def pull_run(work, cmds):
 	        print(result) 
 bot = Client(
   "CW",
-  bot_token="5717007875:AAFuDj6aaKcIT31AZmUQRg2414CcZrjdP-g",
-  api_id=1654363,
-  api_hash="26b911420edb5ceb8f370f21f5eb2684"
+  bot_token="6335459712:AAFG0cDAkVBEJyJWMSGdcDf1ULyWFRPNmIs",
+  api_id=20807000,
+  api_hash="cde2366a7c61e23f4cb44618cbe6cf70"
 )
 
 @bot.on_message(filters.command(["down"]) & ~filters.edited)
